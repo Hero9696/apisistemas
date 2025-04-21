9789b008344b7dd2101a7bb237326fe6fc6becef
Hero9696 <Miguelpadilla9619@gmail.com>
2025-04-21 11:16:05 -0600

    add enpoints Estado Civil

---
+++ backend/endpoints/estadocivil/estadocivilPut.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once __DIR__ . '/../../../api/controllers/put.controller.php';
+header('Content-Type: application/json');
+
+// Solo aceptar método PUT
+if ($_SERVER["REQUEST_METHOD"] == "PUT") {
+
+    // Obtener el contenido del body y convertirlo a array
+    $data = json_decode(file_get_contents("php://input"), true);
+
+    // Validar que venga el ID
+    if (!isset($data["id_EstadoCivil"])) {
+        echo json_encode([
+            "status" => 400,
+            "results" => "Falta el campo id_Especialidad"
+        ], http_response_code(400));
+        return;
+    }
+
+    $id = $data["id_EstadoCivil"];
+    unset($data["id_EstadoCivil"]); // No queremos actualizar el ID
+
+    $table = "Estadocivil";
+    $nameId = "id_EstadoCivil";
+
+    PutController::putData($table, $data, $id, $nameId);
+
+} else {
+    echo json_encode([
+        "status" => 405,
+        "results" => "Método no permitido"
+    ], http_response_code(405));
+}
