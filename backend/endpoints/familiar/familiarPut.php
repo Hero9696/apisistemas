8f43804a51b1aab27f5007a36580fceea2d7a140
Hero9696 <Miguelpadilla9619@gmail.com>
2025-04-21 14:33:58 -0600

    add endpoints familiar

---
+++ backend/endpoints/familiar/familiarPut.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once __DIR__ . '/../../../api/controllers/put.controller.php';
+header('Content-Type: application/json');
+
+// Solo aceptar método PUT
+if ($_SERVER["REQUEST_METHOD"] == "PUT") {
+
+    // Obtener el contenido del body y convertirlo a array
+    $data = json_decode(file_get_contents("php://input"), true);
+
+    // Validar que venga el ID
+    if (!isset($data["id_Familiar"])) {
+        echo json_encode([
+            "status" => 400,
+            "results" => "Falta el campo id_Especialidad"
+        ], http_response_code(400));
+        return;
+    }
+
+    $id = $data["id_Familiar"];
+    unset($data["id_Familiar"]); // No queremos actualizar el ID
+
+    $table = "Familiar";
+    $nameId = "id_Familiar";
+
+    PutController::putData($table, $data, $id, $nameId);
+
+} else {
+    echo json_encode([
+        "status" => 405,
+        "results" => "Método no permitido"
+    ], http_response_code(405));
+}
